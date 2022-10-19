6b77ca5b695e637228f43a7ccffc09f326739318
Pedro Manuel Diaz Santa Maria <pdiazsan@emeal.nttdata.com>
2022-10-19 15:16:32 -0500

    Add Eureka Client

--- src/main/java/com/nttdata/bootcamp/project/Operations/OperationsApplication.java
+++ src/main/java/com/nttdata/bootcamp/project/Operations/OperationsApplication.java
@@ -2,8 +2,10 @@ package com.nttdata.bootcamp.project.Operations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OperationsApplication {
 
 	public static void main(String[] args) {
